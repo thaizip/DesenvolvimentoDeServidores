53e29f4534097b3cba9df98d27d8d5163c1fde2a
oliThai <oliveirath27@gmail.com>
2023-03-08 10:28:03 -0300

    Atividade, class pessoa, suas ações e atributos

--- 08-03/Program.cs
+++ 08-03/Program.cs
@@ -5,3 +5,6 @@ Console.WriteLine("Hello, World!");
 
 produto p1 = new produto(1, "Mouse sem fio", 59.90M);
 Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
+
+pessoa Pessoa1 = new pessoa(1, "Thais", "(16)99623-3113");
+Console.WriteLine(Pessoa1.GetNome() + " " + Pessoa1.GetTelefone());
---
+++ 08-03/pessoa.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_03
+{
+    public class pessoa
+    {
+        private int Id;
+        private string Nome;
+        private string Telefone;
+
+        public pessoa(int id, string nome, string telefone)
+        {
+            Id = id;
+            Nome = nome;
+            Telefone = telefone;
+
+        }
+        
+        public int GetId()
+        {
+            return Id;
+        }
+        public void setId(int id)
+        {
+            Id = id;
+        }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+        public void setNome(string nome)
+        {
+            Nome = nome;
+        }
+
+        public string GetTelefone()
+        {
+            return Telefone;
+        }
+        public void setTelefone(string telefone)
+        {
+            Telefone = telefone;
+        }
+    }
+
+
+
+
+}
