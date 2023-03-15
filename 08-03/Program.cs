81c583828962a6e7d11b3996e2c286192fc51b80
oliThai <oliveirath27@gmail.com>
2023-03-15 10:55:07 -0300

    Criação de lista cliente e lista de veiculos

--- 08-03/Program.cs
+++ 08-03/Program.cs
@@ -14,10 +14,49 @@ Endereco e1 = new Endereco("Avelino Boselli", "162", "15906-300", "Jardim Sobral
 Cliente c1 = new Cliente(1, "Thais", "(16)99623-3113", e1);
 Cliente c2 = new Cliente(2, "Raul", "(16)99679-3978", e1);
 Cliente c3 = new Cliente(3, "Gustavo", "(16)99758-1346", e1);
-Cliente c4 = new Cliente(4, "Samuel", "(16)9815-8293", e1);
+Cliente c4 = new Cliente(4, "Sergio", "(16)9815-8293", e1);
 
 Console.WriteLine(c1.getEndereco().getRua());
 //c4.getEndereco().setRua("Dr.Jose de freitas Madeira");
 //Console.WriteLine(c4.getNome() + " " + c4.getEndereco().getRua());
 Console.WriteLine(c2.getNome() + " " + c2.getEndereco().getEnderecoCompleto());
 Console.WriteLine(c3.getNome()+ " " + c3.getEndereco().getEnderecoCurto());
+
+Carro carro = new Carro();
+carro.Marca = "W";
+carro.NumeroPortas = 4;
+carro.NumeroRodas = 4;
+carro.Cor = "preto";
+
+Aviao aviao = new Aviao();
+aviao.PortaInternas = 10;
+aviao.NumeroPortas = 2;
+aviao.NumeroRodas = 3;
+aviao.Cor = "Branco";
+aviao.QuantidadeAcentos = 150;
+aviao.PagaImposto = false;
+
+Bicicleta bicicleta = new Bicicleta();
+bicicleta.PagaImposto= false;
+bicicleta.Cor = "Vermelha";
+
+Console.WriteLine(c1.getEndereco().getEnderecoCurto());
+Console.WriteLine(aviao.ExibirDados());
+Console.WriteLine(carro.ExibirDados());
+
+List<Veiculos> listaVeiculos  = new List<Veiculos>();
+listaVeiculos.Add(aviao);
+listaVeiculos.Add(carro);
+listaVeiculos.Add(bicicleta);
+
+List<Cliente> listaCliente = new List<Cliente>();
+listaCliente.Add(c1);
+listaCliente.Add(c2);
+listaCliente.Add(c3);
+listaCliente.Add(c4);
+
+
+foreach (Cliente item in listaCliente)
+{
+    Console.WriteLine(item.getNome() + "  Endereço: " + item.getEndereco().getEnderecoCurto());
+}
---
+++ 08-03/Veiculos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_03
+{
+    public class Veiculos
+    {
+        public int NumeroPortas;
+        public int NumeroRodas;
+        public string Cor;
+        public int QuantidadeAcentos;
+        public bool PagaImposto;
+
+
+        public string ExibirDados()
+        {
+            return "Nº Portas: " + NumeroPortas + "  Cor:  " + Cor;
+        }
+    }
+
+}
